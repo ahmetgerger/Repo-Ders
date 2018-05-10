2e5c0da9bba7a6e4edddf3b37230e96e32e793c3
Ahmet Gerger <ahmet.gerger@outlook.com>
2018-05-10 17:25:07 +0300

    yorum satırı --

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -21,7 +21,7 @@ namespace WindowsFormsApp1
         private void button3_Click(object sender, EventArgs e)
         {
 
-           //button3.Text = "hi!";
+           button3.Text = "hi!";
         }
     }
 }
